568e451c7e4b96bccecdb9e0f00372487c822cc5
Mark Chandler <mark@chanchan.io>
2019-09-20 20:48:56 -0400

    add amazing-creature input

--- .github/actions/debug-action/__tests__/debug.test.ts
+++ .github/actions/debug-action/__tests__/debug.test.ts
@@ -1,5 +1,29 @@
+import fs from 'fs'
 import * as core from '@actions/core'
+import yaml from 'js-yaml'
 import run from '../debug'
+
+beforeEach(() => {
+  jest.resetModules()
+
+  const doc = yaml.safeLoad(
+    fs.readFileSync(__dirname + '/../action.yml', 'utf8'),
+  )
+  Object.keys(doc.inputs).forEach(name => {
+    const envVar = `INPUT_${name.replace(/ /g, '_').toUpperCase()}`
+    process.env[envVar] = doc.inputs[name]['default']
+  })
+})
+
+afterEach(() => {
+  const doc = yaml.safeLoad(
+    fs.readFileSync(__dirname + '/../action.yml', 'utf8'),
+  )
+  Object.keys(doc.inputs).forEach(name => {
+    const envVar = `INPUT_${name.replace(/ /g, '_').toUpperCase()}`
+    delete process.env[envVar]
+  })
+})
 
 describe('debug action debug messages', () => {
   it('outputs a debug message', async () => {
